9886f6408db9299f9fba5adb5b41a7fe24499901
John R. Estes <John.R.Estes@icloud.com>
2020-09-02 18:10:00 -0400

    sdded alerts

--- dayTripRev.js
+++ dayTripRev.js
@@ -18,11 +18,10 @@ function dayTripGenerator(){
     displayTrip(curDest,curTrans,curRest,curEntert);
 
     do{
-        console.log("Are you happy with your trip?", "yes or no");
-        response = prompt("Are you happy with your trip?", "yes or no"); 
+        response = prompt("Are you happy with your trip?\nyes or no"); 
         
         if (response === "yes"){
-            console.log("ENJOY!");
+            alert("ENJOY!");
         }
         else {
             let switchResponse = switchResponsePrompt();
@@ -57,16 +56,11 @@ function randomdSelectInput(destTranRestEnt){
 }
 
 function displayTrip(curDest, curTrans, curRest, curEntert){
-    console.log("You are going to " + curDest + " via a " + curTrans);
-    console.log("You will eat at " + curRest + " and go to a " + curEntert);
+    alert("You are going to " + curDest + " via a " + curTrans + ".\nYou will eat at " + curRest + " and go to a " + curEntert);
 }
 
 function switchResponsePrompt(){
     let switchResponse = "";
-    console.log("What change would you like to make?");
-    console.log("1 to change Destination");
-    console.log("2 to change Transportation");
-    console.log("3 to change Restaurant");
-    console.log("4 to change Entertainment");
-    return switchResponse = prompt("What change would you like to make?", "1, 2, 3, or 4");
+
+    return switchResponse = prompt("What change would you like to make?\n\n1 - change Destination\n2 - change Transportation\n3 - change Restaurant\n4 - change Entertainment\n\n1, 2, 3, or 4");
 }
